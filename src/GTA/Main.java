e66895cf5963deb37729c175322127ba87878d56
kirsan07 <terkirsun@gmail.com>
2023-02-03 19:55:55 +0900

    .

--- src/GTA/Main.java
+++ src/GTA/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //String line = scanner.nextLine();
+        String line = scanner.nextLine();
         Reverser reverser = new Reverser() {
 
             @Override
@@ -20,7 +20,7 @@ public class Main {
 
             }
         };
-System.out.println(reverser.reverse("line"));
+System.out.println(reverser.reverse(line));
 
     }
 }
